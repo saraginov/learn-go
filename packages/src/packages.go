07e47d8e42b90b93acb75ad60f0b7353cabdec13
heraclea <konstantin.saraginov@gmail.com>
2019-10-15 09:12:47 -0400

    imports

--- packages/src/packages.go
+++ packages/src/packages.go
@@ -1,5 +1,14 @@
+/*
+	Every Go program is made up of packages.
+	Programs start running in package main.
+*/
 package main
 
+/*
+	This program is using the packages with import paths "fmt" and "math/rand".
+	By convention, the package name is the same as the last element of the import path.
+	For instance, the "math/rand" package comprises files that begin with the statement package rand. 
+*/
 import (
 	"fmt"
 	"math/rand"
