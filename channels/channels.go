7f835deaff0b2992ef02b0ed4e75783959e34de2
heraclea <konstantin.saraginov@gmail.com>
2019-11-10 19:00:28 -0500

    channel range and close

--- channels/channels.go
+++ channels/channels.go
@@ -118,7 +118,53 @@ func bufferedChannels() {
 	// }
 }
 
+/*
+	Range and Close
+
+		A sender can close a channel to indicate that no more values will be sent.
+
+		Receivers can test whether a channel has been closed by assigning a second
+		parameter to the receive expression: after
+
+		v, ok := <-ch
+
+			ok is false if there are no more values to receive and the channel is closed.
+
+		The loop for i := range c receives values from the channel repeatedly until
+		it is closed.
+
+		NOTE:
+			Only the sender should close a channel, never the receiver. Sending on a closed
+			channel will cause a panic.
+
+			Channels are not like files! You don't usually need to close them.
+			Closing channels is only necessary when the receiver must be told there are no
+			more values coming, such as to terminate a range loop
+*/
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+
+	close(c)
+}
+
+func rangeAndClose() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c) // cap for channel c is 10 bytes, thus n in func fibonacci is 10
+
+	for i := range c {
+		fmt.Println(i)
+	}
+
+}
+
 func main() {
 	channels()
 	bufferedChannels()
+	rangeAndClose()
 }
