53b5cb2285b6604c15596af9b3dfa8d3e0a7e1cb
heraclea <konstantin.saraginov@gmail.com>
2019-11-16 16:21:35 -0500

    unsaved changes to binary-tree

--- exercise-equivalent-binary-trees/binary-tree.go
+++ exercise-equivalent-binary-trees/binary-tree.go
@@ -56,6 +56,7 @@ func main() {
 	t := tree.New(1)
 
 	go func() {
+		//
 		defer close(c)
 		Walk(t, c)
 	}()
@@ -67,6 +68,7 @@ func main() {
 }
 
 // https://github.com/golang/tour/blob/master/tree/tree.go#L20
+// constructs a randomly-structured (but always sorted) binary tree holding the values
 // Does not return a random binary tree ... always returns identical tree structure
 
 // where k = 1, someTree is always :
@@ -77,46 +79,10 @@ func main() {
 
 // where k =3
 // ((((3 (6)) 9 (12)) 15 ((18) 21 ((24) 27))) 30)
-
 // ... and so on
 
-/**
-
-type Tree struct {
-    Left  *Tree
-    Value int
-    Right *Tree
-}
-
-todo:
-	walk along tree, send to ch <- value we just read
-
-	close channel once all values have been read
-
-	get starting value, add value to array/list
-	get left tree
-	get right tree
-
-	get starting value of left tree
-	get left left tree
-	get left right tree
-
-	get starting value of left left tree
-	get left left left tree
-	get left left right tree
-
-	get starting value of left left left tree
-	assuming left left left left tree is empty
-	assuming left left left right tree is empty
-
-	go back up a branch (level)
-	get starting value for left left right tree
-	assuming left left right left tree is empty
-	assuming left left right right tree is empty
-
-	go back up a branch, both left and right of this node/branch/level explored
-	get starting value of left right tree
-	get left right left tree
-	get left right right tree
-
-*/
+// type Tree struct {
+//     Left  *Tree
+//     Value int
+//     Right *Tree
+// }
